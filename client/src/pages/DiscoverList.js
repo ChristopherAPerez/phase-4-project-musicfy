6f7e2f504687d2c50b30ea8673447b972ad4ec23
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2023-01-19 03:39:39 -0500

    Update

--- client/src/pages/DiscoverList.js
+++ client/src/pages/DiscoverList.js
@@ -3,7 +3,9 @@ import React from "react";
 function DiscoverList({ album }) {
     return (
         <>
-            <img src={album.artist} alt={album.album_image} />
+            <img src={album.artist_image} alt={album.artist_image} width="200" height="250"/>
+            <h1>{album.artist}</h1>
+            <img src={album.album_image} alt={album.album_image} width="50" height="50"/>
             <table>
                 
             </table>
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,6 @@ because_the_internet = Album.create(album_title: "Because The Internet",
                                     artist_image: "https://m.media-amazon.com/images/M/MV5BNzUxNTU5ODkxNl5BMl5BanBnXkFtZTgwOTIyNjc5MDI@._V1_.jpg",
                                     album_length: 3472,
                                     likes: 8005)
-
-camp = Album.create(album_title: "Camp",
-                                    artist: "Childish Gambino",
-                                    genre: "Hip-Hop",
-                                    album_image: "https://media.pitchfork.com/photos/5929a4ce13d1975652138dc8/1:1/w_450%2Cc_limit/a61ee51a.jpg",
-                                    artist_image: "https://m.media-amazon.com/images/M/MV5BNzUxNTU5ODkxNl5BMl5BanBnXkFtZTgwOTIyNjc5MDI@._V1_.jpg",
-                                    album_length: 3472,
-                                    likes: 8005)
                     
 the_click = Album.create(album_title: "The Click",
                                     artist: "AJR",
@@ -52,7 +44,7 @@ blurryface = Album.create(album_title: "Blurryface",
                                     artist: "Twenty One Pilots",
                                     genre: "Alternative Pop",
                                     album_image: "https://upload.wikimedia.org/wikipedia/en/7/7d/Blurryface_by_Twenty_One_Pilots.png",
-                                    artist_image: "https://m.media-amazon.com/images/M/MV5BNzUxNTU5ODkxNl5BMl5BanBnXkFtZTgwOTIyNjc5MDI@._V1_.jpg",
+                                    artist_image: "https://www.washingtonpost.com/rf/image_1484w/2010-2019/WashingtonPost/2016/05/31/Style/Images/twentyonepilots.promo4-JabariJacobs2.jpg?t=20170517",
                                     album_length: 3143,
                                     likes: 8087)
 
@@ -60,13 +52,12 @@ vessel = Album.create(album_title: "Vessel",
                                     artist: "Twenty One Pilots",
                                     genre: "Alternative Pop",
                                     album_image: "https://upload.wikimedia.org/wikipedia/en/2/20/Vessel_by_Twenty_One_Pilots.jpg",
-                                    artist_image: "https://m.media-amazon.com/images/M/MV5BNzUxNTU5ODkxNl5BMl5BanBnXkFtZTgwOTIyNjc5MDI@._V1_.jpg",
+                                    artist_image: "https://www.washingtonpost.com/rf/image_1484w/2010-2019/WashingtonPost/2016/05/31/Style/Images/twentyonepilots.promo4-JabariJacobs2.jpg?t=20170517",
                                     album_length: 2864,
                                     likes: 10355)
 
 puts "🏕 Seeding songs..."
 sweatpants = Song.create(song_title: "IV.Sweatpants", feature: "Problem", song_length: 181, streams: 9887, user_id: donald_glover.id, album_id: because_the_internet.id)
-bonfire = Song.create(song_title: "Bonfire", feature: nil, song_length: 235, streams: 3005, user_id: donald_glover.id, album_id: camp.id)
 weak = Song.create(song_title: "Weak", feature: nil, song_length: 201, streams: 3213, user_id: adam_met.id, album_id: the_click.id)
 karma = Song.create(song_title: "Karma", feature: nil, song_length: 245, streams: 9052, user_id: jack_met.id, album_id: neotheater.id)
 ordinary_people = Song.create(song_title: "Ordinaryish People", feature: "Blue Man Group", song_length: 219, streams: 2109, user_id: ryan_met.id, album_id: ok.id)
